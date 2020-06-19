dc9eed04c75b2ef7d7c787332390e0d4c07eab09
Vince Williams <williamsvm20@gmail.com>
2020-06-19 12:45:00 -0400

    home page header implmented

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Form from './Form'
 import Home from './Home'
 import Pizza from './Pizza.js'
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Link} from 'react-router-dom'
 
 const App = () => {
 
@@ -10,16 +10,20 @@ const App = () => {
 
   return (
     <div>
+      <header>
+        <h1>Lambda Eats</h1>
+        <Link to='/' >Home</Link>
+      </header>
       <Switch>
-        <Route>
+        <Route path='/order/:id'>
           <Pizza />
         </Route>
 
-        <Route>
+        <Route path ='/order'>
           <Form />
         </Route>
 
-        <Route>
+        <Route path='/'>
           <Home />
         </Route>
       </Switch>
